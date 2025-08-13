599ab6d6b055f086b722c03164503a69c17f614e
Ellie Alavi <elhamalavi.f@gmail.com>
2025-08-13 18:15:35 -0400

    Revised Header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import apiLogo from '../assets/ApiLogo.png';
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo">API LOGO</div>
+      <div className="logo">
+        <img src={apiLogo} alt="API Logo" className="logo-img" />
+      </div>
       <nav className="nav-buttons">
         <Link to="/" className="nav-button-link">
           Home
